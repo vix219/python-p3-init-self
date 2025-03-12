45f1346013b2c4588c02c58f1c29c9aa79f53d74
VictoriaCarvalho <vix219@icloud.com>
2025-03-12 11:30:30 -0500

    add lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
+
+
 class Dog:
-    pass
+    def __init__(self, name, breed= "Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+dog = Dog("Fido")  
+breed = Dog("Dalmatian")
+print(dog.name)
+print(dog.breed)
+
+
+
+
+
+
